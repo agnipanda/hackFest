dabcfbd509ffa66642144286539ff6a2592b0d5b
agnipanda <agnipanda69@gmail.com>
2018-03-11 11:42:30 +0530

    Update app.js

--- myapp/app.js
+++ myapp/app.js
@@ -8,8 +8,7 @@ var mongoose = require('mongoose');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var logsign = require('./routes/logsign');
-var api = require('./routes/api');
-
+var Schema = mongoose.Schema;
 var app = express();
 mongoose.connect('mongodb://localhost/sdata');
 // view engine setup
@@ -28,12 +27,43 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/logsign', logsign);
-app.use('/api',api);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
+});
+var studSchema = new Schema({
+	name: {type : String , required : true},
+	email: {type : String , required : true},
+	phone: {type : String , required : true},
+	roomid: {type : String , required : true},
+	pasword: {type : String , required : true},
+});
+var studModel = mongoose.model('studModel',studSchema);
+
+app.post('/logsign', function(req, res){
+   var studInfo=req.body; //Get the parsed information
+   
+   if(!studInfo.name || !studInfo.email || !studInfo.phone || !studInfo.roomid || !studInfo.password){
+      res.render('show_message', {
+         message: "Sorry, you provided worng info", type: "error"});
+   } else {
+      var newStud=new studModel({
+		name:studInfo.name,
+		email:studInfo.email,
+		phone:studInfo.phone,
+		roomid:studInfo.roomid,
+		password:studInfo.password
+		});
+      newStud.save(function(err, studModel){
+         if(err)
+            res.render('show_message', {message: "Database error", type: "error"});
+         else
+            res.render('show_message', {
+               message: "New student added", type: "logsign", studModel: studInfo});
+      });
+   };
 });
 
 // error handler
